598f09e8a7823fa2edda4d39312eb93299a328f7
PabloSzx <pablosaez1995@gmail.com>
2019-06-20 21:54:30 -0400

    improve index exports

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-import decrypt from './decrypt';
-import decrypt_object from './decrypt_object';
-import encrypt from './encrypt';
-import encrypt_object from './encrypt_object';
-import get_key from './get_key';
-import new_key from './new_key';
-import KeyManager from './key_manager';
-import { validate } from './utils';
-
-export {
-  encrypt,
-  encrypt_object,
-  decrypt,
-  decrypt_object,
-  get_key,
-  new_key,
-  validate,
-  KeyManager,
-};
+export { default as decrypt } from './decrypt';
+export { default as decrypt_object } from './decrypt_object';
+export { default as encrypt } from './encrypt';
+export { default as encrypt_object } from './encrypt_object';
+export { default as get_key } from './get_key';
+export { default as new_key } from './new_key';
+export { default as KeyManager } from './key_manager';
+export { validate } from './utils';
